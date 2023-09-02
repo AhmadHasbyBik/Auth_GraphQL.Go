79ad4f94d96133ed1f043083e7c88bc9336c0217
AhmadHasbyBik <abik262002@gmail.com>
2023-09-02 10:24:06 +0700

    graphql

--- server.go
+++ server.go
@@ -18,7 +18,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
+	graph.Database()
 	port := os.Getenv("PORT")
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
